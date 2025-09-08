6985ff11bee42430303094ca1ee4a9035b9f1d31
EmmanuelOdigie <emmanuelodigie1@outlook.com>
2025-09-08 10:24:12 +0100

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,19 @@
+package JavaSquareRoot; // project created in eclipse
+
+public class SquareRoot { // public class created 'SquareRoot'
+
+	public static void main(String[] args) {// read,runs & executes code
+		double squareRoot = Math.sqrt(4); // created a double variable 'squareRoot' set it to 
+		// calculate the squareroot of 4, 'Math.sqrt(4)' 
+		
+		int myResult = (int) squareRoot; // converted double to int
+		
+		System.out.println(myResult); // display result	
+		
+
+	}
+
+}
+
+// result:
+// 2
